272eb152a56f14c8b8292ca1ecbaa9f720525d34
Emil Køhler Warmdahl <ekw@concordium.com>
2024-11-05 11:28:25 +0100

    Add constraints to cabal install

--- scripts/ghc-alpine-build.sh
+++ scripts/ghc-alpine-build.sh
@@ -5,7 +5,7 @@ set -ex
 . "$HOME/.ghcup/env"
 
 cd ghc/
-cabal install happy-1.20 alex --install-method=copy
+cabal install happy alex --install-method=copy --constraint 'happy < 1.20'
 cp ~/.cabal/bin/* /usr/bin
 
 # Build GHC
