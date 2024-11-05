cf9e1096cac81057b96e57dcdea5026632e6be1b
Emil Køhler Warmdahl <ekw@concordium.com>
2024-11-05 09:26:22 +0100

    Add workflow to build ghc-alpine

---
+++ scripts/ghc-alpine-build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -ex
+
+. "$HOME/.ghcup/env"
+
+cd ghc/
+cabal install happy alex --install-method=copy
+cp ~/.cabal/bin/* /usr/bin
+
+# Build GHC
+./boot
+./configure --disable-numa
+./hadrian/build -j17 --docs=none
+mkdir _build/docs
+./hadrian/build binary-dist --docs=none
+
+# Copy ghc to out
+ls _build
+ls _build/bindist
+cp _build/bindist/ghc-"$GHC_VERSION"-x86_64-unknown-linux.tar.xz /build/dpkg-build-package/ghc-"$GHC_VERSION"-x86_64-unknown-linux-integer-gmp.tar.xz
