04eb9c838c034d7e997a27dfe21e8ae16b4e6be4
Emil Køhler Warmdahl <ekw@concordium.com>
2024-12-19 14:30:51 +0100

    Use original name for artifact

--- scripts/ghc-alpine-build.sh
+++ scripts/ghc-alpine-build.sh
@@ -5,4 +5,4 @@ set -ex
 docker build -t concordium/alpine-ghc -f docker/ghc-alpine.Dockerfile --build-arg BOOTSTRAP_HASKELL_GHC_VERSION="$BOOTSTRAP_HASKELL_GHC_VERSION" --build-arg GHC_VERSION="$GHC_VERSION" .
 mkdir out
 docker run --name alpine-ghc -e GHC_VERSION -v "$(pwd)"/out:/out concordium/alpine-ghc
-aws s3 cp out/ghc-"$GHC_VERSION"-x86_64-unknown-linux-integer-gmp.tar.xz s3://static-libraries.concordium.com/alpine-tester.tar.xz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp out/ghc-"$GHC_VERSION"-x86_64-unknown-linux-integer-gmp.tar.xz s3://static-libraries.concordium.com/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
